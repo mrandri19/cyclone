5c18dda40c97bdc2fe319e691fe3da34637e0d6b
Andrea Cognolato <andrecogno@hotmail.it>
2021-11-29 22:46:44 +0200

    Train with database data rather than csv

--- bike-rental-regression/train.py
+++ bike-rental-regression/train.py
@@ -1,10 +1,28 @@
+import argparse
+import os
 from argparse import Namespace
-import pandas as pd
+
 import mlflow
+import pandas as pd
+import sqlalchemy
 from sklearn.ensemble import HistGradientBoostingRegressor as Model
 from sklearn.metrics import r2_score
+from sqlalchemy import Column, Integer, JSON, Text, TIMESTAMP
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-import argparse
+Base = declarative_base()
+
+
+class Prediction(Base):
+    __tablename__ = "prediction"
+    id = Column(Integer(), primary_key=True)
+    input = Column(JSON())
+    prediction = Column(JSON())
+    model_id = Column(Text())
+    label = Column(JSON())
+    created_at = Column(TIMESTAMP(timezone=True))
 
 
 def time_features(input_df):
@@ -15,40 +33,44 @@ def time_features(input_df):
     )
 
 
-def main(params: Namespace):
-    with mlflow.start_run():
-        dataset = (
-            pd.read_csv(params.dataset_path, header=0,  sep=',',
-                        parse_dates=['datetime'], index_col='datetime')
-            .drop(
-                columns=['casual', 'registered']
-            ))
-        train = dataset.loc[
-            lambda df: (df.index.year == params.train_year) & (
-                df.index.month == params.train_month)
-        ]
-        test = dataset.loc[
-            lambda df: (df.index.year == params.test_year) & (
-                df.index.month == params.test_month)
-        ]
+def main(params: Namespace, db_uri):
+    print(db_uri)
 
+    engine = create_engine(db_uri)
+
+    query = f"""
+        select *
+        from prediction
+        order by created_at desc
+        limit {params.num_train + params.num_test}
+    """
+
+    df = pd.read_sql(query, con=engine)
+    df = pd.concat([pd.DataFrame(df['input'].tolist()), df['label']], axis=1)
+
+    test = df.iloc[:params.num_test]
+    train = df.iloc[params.num_test:(params.num_test+params.num_train)]
+
+    with mlflow.start_run():
         # Instantiate model
         model = Model()
         mlflow.set_tag("model_module", model.__class__.__module__)
         mlflow.set_tag("model_type", model.__class__.__name__)
+        mlflow.log_param("num_train", params.num_train)
+        mlflow.log_param("num_test", params.num_test)
 
         # Train model
-        model.fit(train.drop(columns='count').pipe(
-            time_features), train['count'])
+        model.fit(train.drop(columns='label'), train['label'])
 
         # Test model
-        score = r2_score(test['count'], model.predict(
-            test.drop(columns='count').pipe(time_features)))
+        score = r2_score(
+            test['label'],
+            model.predict(test.drop(columns='label'))
+        )
         mlflow.log_metric('test_r2_score', score)
 
         # Register trained model
-        input_example = train.head(10).pipe(
-            time_features).drop(columns='count')
+        input_example = train.head(10).drop(columns='label')
         signature = mlflow.models.signature.infer_signature(
             input_example, model.predict(input_example)
         )
@@ -58,12 +80,18 @@ def main(params: Namespace):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('train_year', type=int)
-    parser.add_argument('train_month', type=int)
-    parser.add_argument('test_year', type=int)
-    parser.add_argument('test_month', type=int)
-    parser.add_argument('dataset_path', type=str)
+    parser.add_argument('num_train', type=int)
+    parser.add_argument('num_test', type=int)
 
     args = parser.parse_args()
 
-    main(args)
+    db_uri = sqlalchemy.engine.url.URL.create(
+        drivername="postgresql+psycopg2",
+        username=os.environ["DB_USER"],
+        password=os.environ["DB_PASS"],
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        database=os.environ["DB_NAME"]
+    )
+
+    main(args, db_uri)
